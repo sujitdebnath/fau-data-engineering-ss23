31dafb3c53f48f52603c0fa1b30722f1b94d478c
root <root@sandwichkernel>
2023-06-14 03:51:58 +0200

    Fixed the checking exit code snippet in tests.sh

--- project/tests.sh
+++ project/tests.sh
@@ -3,6 +3,7 @@
 # Run component tests
 echo "----------------------------------- Component Testing Started -----------------------------------"
 pdm run python -m unittest tests/test_component.py
+component_exit_code=$?
 echo "----------------------------------- Component Testing Ended -----------------------------------"
 
 echo ""
@@ -10,16 +11,18 @@ echo ""
 # Run system tests
 echo "----------------------------------- System Level Testing Started -----------------------------------"
 pdm run python -m unittest tests/test_pipeline.py
+system_exit_code=$?
 echo "----------------------------------- System Level Testing Ended -----------------------------------"
 
 echo ""
 
 # Check the exit code
-exit_code=$?
-if [ $exit_code -eq 0 ]; then
+if [ $component_exit_code -eq 0 ] && [ $system_exit_code -eq 0 ]; then
     echo "All tests passed!"
+    exit_code=0
 else
     echo "Some tests failed!"
+    exit_code=1
 fi
 
 # Exit with the appropriate code
